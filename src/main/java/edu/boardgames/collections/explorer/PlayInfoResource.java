90ae44e864fd6f41fe3bc618204b43443cb57a85
Erwin Dupont <erwin.dupont@aca-it.be>
2020-07-20 21:07:39 +0200

    Initial version of the PlaysRequest

--- src/main/java/edu/boardgames/collections/explorer/PlayInfoResource.java
+++ src/main/java/edu/boardgames/collections/explorer/PlayInfoResource.java
@@ -1,10 +1,15 @@
 package edu.boardgames.collections.explorer;
 
 import edu.boardgames.collections.explorer.infrastructure.bgg.BggInit;
+import edu.boardgames.collections.explorer.infrastructure.bgg.PlaysRequest;
 import edu.boardgames.collections.explorer.infrastructure.bgg.ThingRequest;
+import edu.boardgames.collections.explorer.infrastructure.xml.XmlInput;
+import edu.boardgames.collections.explorer.infrastructure.xml.XmlNode;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.util.Arrays;
@@ -18,6 +23,7 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/playinfo")
 public class PlayInfoResource {
+	private static final Logger LOGGER = System.getLogger(PlayInfoResource.class.getName());
 
 	@GET
 	@Path("/xsl")
@@ -35,7 +41,7 @@ public class PlayInfoResource {
 		String xsl = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
 				"<?xml-stylesheet type=\"text/xsl\" href=\"http://localhost:8080/playinfo/xsl\"?>\n";
 		String result = String.format("%s%n%s", xsl, StringUtils.removeStartIgnoreCase(response, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"));
-		System.out.printf("%s%n", result);
+		LOGGER.log(Level.INFO, result);
 		return result;
 	}
 
@@ -47,4 +53,22 @@ public class PlayInfoResource {
 			    .map(BoardGameRender::playInfo)
 			    .collect(Collectors.joining(System.lineSeparator()));
     }
+
+	@GET
+	@Path("/plays/{username}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String userPlays(@PathParam("username") String username) {
+		return new PlaysRequest().username(username)
+		                         .asInputStreams()
+		                         .map(new XmlInput()::read)
+		                         .flatMap(root -> XmlNode.nodes(root, "//play"))
+		                         .map(node -> new XmlNode(node) {
+			                         public String render() {
+				                         return String.format("Id: %s - Date: %s - Location: %s", this.string("@id"), this.string("@date"), this.string("@location"));
+			                         }
+		                         })
+		                         .map(xmlNode -> xmlNode.render())
+		                         .sorted()
+		                         .collect(Collectors.joining(System.lineSeparator()));
+	}
 }
---
+++ src/main/java/edu/boardgames/collections/explorer/infrastructure/bgg/PlaysRequest.java
@@ -0,0 +1,97 @@
+package edu.boardgames.collections.explorer.infrastructure.bgg;
+
+import edu.boardgames.collections.explorer.infrastructure.Async;
+import edu.boardgames.collections.explorer.infrastructure.xml.XmlInput;
+import edu.boardgames.collections.explorer.infrastructure.xml.XmlNode;
+
+import java.io.InputStream;
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+import java.util.NoSuchElementException;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class PlaysRequest extends BggRequest<PlaysRequest> {
+	private static final Logger LOGGER = System.getLogger(PlaysRequest.class.getName());
+
+	private int page = 0;
+
+	public PlaysRequest() {
+		super(BggApi.V2.create("plays"));
+		this.addOption("type", "thing");
+	}
+
+	@Override
+	PlaysRequest self() {
+		return this;
+	}
+
+	public PlaysRequest username(String username) {
+		this.addOption("username", username);
+		return this;
+	}
+
+	public PlaysRequest id(String id) {
+		this.addOption("id", id);
+		return this;
+	}
+
+	public PlaysRequest page(int page) {
+		this.page = page;
+		this.addOption("page", Integer.toString(page));
+		return this;
+	}
+
+	@Override
+	public InputStream asInputStream() {
+		return this.asInputStreams()
+		           .reduce(PlaysRequest::onlyOne)
+		           .orElseThrow(() -> new NoSuchElementException("Exactly one element expected."));
+	}
+
+	private static <T> T onlyOne(T first, T second) {
+		throw new IllegalArgumentException("Only one element expected.");
+	}
+
+	public Stream<InputStream> asInputStreams() {
+		return Async.map(this.createPagedRequests(), PlaysRequest::superAsInputStream);
+	}
+
+	private InputStream superAsInputStream() {
+		return super.asInputStream();
+	}
+
+	@Override
+	public Stream<String> asLines() {
+		return Async.map(this.createPagedRequests(), PlaysRequest::superAsLines)
+		            .flatMap(Function.identity());
+	}
+
+	private Stream<String> superAsLines() {
+		return super.asLines();
+	}
+
+	private Stream<PlaysRequest> createPagedRequests() {
+		if (this.page > 0) {
+			LOGGER.log(Level.DEBUG, "Page {0, number, integer} defined, returning this request", this.page);
+			return Stream.of(this);
+		} else {
+			LOGGER.log(Level.DEBUG, "No page defined, fetching total number of plays", this.page);
+			Integer total = XmlNode.nodes(new XmlInput().read(this.superAsInputStream()), "/plays")
+			                         .findFirst()
+			                         .map(node -> new XmlNode(node) {
+				                         public int total() {
+					                         return super.number("@total")
+					                                     .intValue();
+				                         }
+			                         })
+			                         .map(xmlNode -> xmlNode.total())
+			                         .orElse(0);
+			int pages = (total / 100) + 1;
+			LOGGER.log(Level.DEBUG, "Performing {0, number, integer} paged requests to fetch {1, number, integer} plays", pages, total);
+			return IntStream.rangeClosed(1, pages)
+					.mapToObj(page -> this.copy(PlaysRequest::new).page(page));
+		}
+	}
+}
