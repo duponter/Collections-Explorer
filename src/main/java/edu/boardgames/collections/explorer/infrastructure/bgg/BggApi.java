d0e70df5f832c580b77ff830288955078f45b5a4
Erwin Dupont <erwin.dupont@aca-it.be>
2022-01-28 07:55:12 +0100

    Fixed API base url's

--- src/main/java/edu/boardgames/collections/explorer/infrastructure/bgg/BggApi.java
+++ src/main/java/edu/boardgames/collections/explorer/infrastructure/bgg/BggApi.java
@@ -3,8 +3,8 @@ package edu.boardgames.collections.explorer.infrastructure.bgg;
 import java.net.URI;
 
 public enum BggApi {
-	V1("https://www.boardgamegeek.com/xmlapi/"),
-	V2("https://www.boardgamegeek.com/xmlapi2/");
+	V1("https://boardgamegeek.com/xmlapi/"),
+	V2("https://boardgamegeek.com/xmlapi2/");
 
 	private final String baseUrl;
 
@@ -13,6 +13,6 @@ public enum BggApi {
 	}
 
 	public BggUrlFactory create(String subpath) {
-		return queryString -> URI.create(String.format("%s%s?%s", baseUrl, subpath, queryString));
+		return queryString -> URI.create("%s%s?%s".formatted(baseUrl, subpath, queryString));
 	}
 }
