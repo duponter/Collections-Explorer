d8a1a2d01d2f26d2f557fed64d24de1cc73b75c4
Erwin Dupont <erwin.dupont@aca-it.be>
2022-03-30 14:25:05 +0200

    Converted to Java Record

--- src/main/java/edu/boardgames/collections/explorer/domain/Copy.java
+++ src/main/java/edu/boardgames/collections/explorer/domain/Copy.java
@@ -2,20 +2,9 @@ package edu.boardgames.collections.explorer.domain;
 
 import java.util.Objects;
 
-public final class Copy {
-	private final BoardGame boardGame;
-	private final BoardGameCollection collection;
-
-	public Copy(BoardGame boardGame, BoardGameCollection collection) {
-		this.boardGame = Objects.requireNonNull(boardGame);
-		this.collection = Objects.requireNonNull(collection);
-	}
-
-	public BoardGame boardGame() {
-		return boardGame;
-	}
-
-	public BoardGameCollection collection() {
-		return collection;
-	}
+public record Copy(BoardGame boardGame, BoardGameCollection collection) {
+    public Copy(BoardGame boardGame, BoardGameCollection collection) {
+        this.boardGame = Objects.requireNonNull(boardGame);
+        this.collection = Objects.requireNonNull(collection);
+    }
 }
--- src/main/java/edu/boardgames/collections/explorer/infrastructure/bgg/GeekBuddyBgg.java
+++ src/main/java/edu/boardgames/collections/explorer/infrastructure/bgg/GeekBuddyBgg.java
@@ -2,34 +2,20 @@ package edu.boardgames.collections.explorer.infrastructure.bgg;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.StringJoiner;
 
 import edu.boardgames.collections.explorer.domain.BoardGame;
 import edu.boardgames.collections.explorer.domain.GeekBuddy;
 
-public class GeekBuddyBgg implements GeekBuddy {
-	private final String username;
-	private final String name;
+public record GeekBuddyBgg(String username, String name) implements GeekBuddy {
+    public GeekBuddyBgg(String username, String name) {
+        this.username = Objects.requireNonNull(username);
+        this.name = Objects.requireNonNull(name);
+    }
 
-	GeekBuddyBgg(String username, String name) {
-		this.username = Objects.requireNonNull(username);
-		this.name = Objects.requireNonNull(name);
-	}
-
-	@Override
-	public String username() {
-		return username;
-	}
-
-	@Override
-	public String name() {
-		return name;
-	}
-
-	@Override
-	public List<BoardGame> ownedCollection() {
-		return BggInit.get().geekBuddyCollections().owned(this).boardGames();
-	}
+    @Override
+    public List<BoardGame> ownedCollection() {
+        return BggInit.get().geekBuddyCollections().owned(this).boardGames();
+    }
 
     @Override
     public List<BoardGame> preorderedCollection() {
@@ -51,29 +37,21 @@ public class GeekBuddyBgg implements GeekBuddy {
         return BggInit.get().geekBuddyCollections().rated(this).boardGames();
     }
 
-	@Override
-	public List<BoardGame> ratedCollection(int minrating) {
-		return BggInit.get().geekBuddyCollections().minimallyRated(this, minrating).boardGames();
-	}
+    @Override
+    public List<BoardGame> ratedCollection(int minrating) {
+        return BggInit.get().geekBuddyCollections().minimallyRated(this, minrating).boardGames();
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		GeekBuddyBgg that = (GeekBuddyBgg) o;
-		return username.equals(that.username);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GeekBuddyBgg that = (GeekBuddyBgg) o;
+        return username.equals(that.username);
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(username);
-	}
-
-	@Override
-	public String toString() {
-		return new StringJoiner(", ", GeekBuddyBgg.class.getSimpleName() + "[", "]")
-				.add(String.format("username='%s'", username))
-				.add(String.format("name='%s'", name))
-				.toString();
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
 }
