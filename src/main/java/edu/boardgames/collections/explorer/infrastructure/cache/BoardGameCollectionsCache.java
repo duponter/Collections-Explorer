1f3c4998eee029eb6e1dc6939cae457572a49faa
Erwin Dupont <erwin.dupont@aca-it.be>
2022-04-04 21:01:30 +0200

    Replaced LazyBoardGameCollection with BoardGameCollection returned from GeekBuddy or GeekList

--- src/main/java/edu/boardgames/collections/explorer/infrastructure/cache/BoardGameCollectionsCache.java
+++ src/main/java/edu/boardgames/collections/explorer/infrastructure/cache/BoardGameCollectionsCache.java
@@ -1,42 +1,65 @@
 package edu.boardgames.collections.explorer.infrastructure.cache;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import edu.boardgames.collections.explorer.domain.BoardGame;
 import edu.boardgames.collections.explorer.domain.BoardGameCollection;
 import edu.boardgames.collections.explorer.domain.BoardGameCollectionGroup;
 import edu.boardgames.collections.explorer.domain.BoardGameCollections;
 import edu.boardgames.collections.explorer.domain.GeekBuddies;
+import edu.boardgames.collections.explorer.domain.GeekBuddy;
 import edu.boardgames.collections.explorer.domain.GeekLists;
 import io.vavr.Lazy;
 
 public class BoardGameCollectionsCache implements BoardGameCollections {
-	private final Cache<String, BoardGameCollection> collections = Caffeine.newBuilder().build();
+    private final Cache<String, Lazy<BoardGameCollection>> collections = Caffeine.newBuilder().build();
 
 	public BoardGameCollectionsCache(GeekBuddies geekBuddies, GeekLists geekLists) {
-        geekBuddies.all().forEach(geekBuddy -> collections.put(StringUtils.lowerCase(geekBuddy.username()), new LazyBoardGameCollection(geekBuddy.username(), geekBuddy.name(), Lazy.of(() -> geekBuddy.ownedCollection().boardGames()))));
-		geekLists.all().forEach(geekList -> collections.put(geekList.id(), new LazyBoardGameCollection(geekList.id(), geekList.id(), Lazy.of(() -> geekList.asCollection().boardGames()))));
+        geekBuddies.all().forEach(geekBuddy -> collections.put(StringUtils.lowerCase(geekBuddy.username()), Lazy.of(() -> fromGeekBuddy(geekBuddy))));
+        geekLists.all().forEach(geekList -> collections.put(geekList.id(), Lazy.of(geekList::asCollection)));
 
-		collections.put("mine", this.asGroup("mine", "duponter", "274761"));
-		collections.put("bareelstraat", this.asGroup("bareelstraat", "mine", "wouteraerts", "jarrebesetoert"));
-		collections.put("fmlimited", this.asGroup("fmlimited", "mine", "bartie", "de rode baron", "edou", "evildee", "svennos", "turtler6"));
-		collections.put("sirplayalot", this.asGroup("sirplayalot", "wallofshame", "leys"));
+        collections.put("mine", Lazy.of(() -> this.asGroup("mine", "duponter", "274761")));
+        collections.put("bareelstraat", Lazy.of(() -> this.asGroup("bareelstraat", "mine", "wouteraerts", "jarrebesetoert")));
+        collections.put("fmlimited", Lazy.of(() -> this.asGroup("fmlimited", "mine", "bartie", "de rode baron", "edou", "evildee", "svennos", "turtler6")));
+        collections.put("sirplayalot", Lazy.of(() -> this.asGroup("sirplayalot", "wallofshame", "leys")));
 //		collections.put("forum", this.asGroup("forum", "forummortsel", "ffed"));
-	}
+    }
 
-	@Override
-	public BoardGameCollection all() {
-		return new BoardGameCollectionGroup("all", collections.asMap()
-		                                                      .values()
-		                                                      .toArray(BoardGameCollection[]::new));
-	}
+    private BoardGameCollection fromGeekBuddy(GeekBuddy geekBuddy) {
+        BoardGameCollection owned = geekBuddy.ownedCollection();
+        return new BoardGameCollection() {
+            @Override
+            public String id() {
+                return owned.id();
+            }
 
-	@Override
-	public BoardGameCollection one(String name) {
-		return collections.getIfPresent(StringUtils.lowerCase(name));
+            @Override
+            public String name() {
+                return geekBuddy.name();
+            }
+
+            @Override
+            public List<BoardGame> boardGames() {
+                return owned.boardGames();
+            }
+        };
+    }
+
+    @Override
+    public BoardGameCollection all() {
+        return new BoardGameCollectionGroup("all", collections.asMap()
+                .values()
+                .toArray(BoardGameCollection[]::new));
+    }
+
+    @Override
+    public BoardGameCollection one(String name) {
+        return collections.getIfPresent(StringUtils.lowerCase(name)).get();
 	}
 
 	@Override
