773caa568a71b11ac4f61fc4d8f012a79ff9dec2
Erwin Dupont <erwin.dupont@aca-it.be>
2022-04-07 18:20:52 +0200

    GeekBuddyCollectionsCache now caches all filtered collections, and not only owned and want-to-play

--- src/main/java/edu/boardgames/collections/explorer/infrastructure/cache/GeekBuddyCollectionsCache.java
+++ src/main/java/edu/boardgames/collections/explorer/infrastructure/cache/GeekBuddyCollectionsCache.java
@@ -1,67 +1,58 @@
 package edu.boardgames.collections.explorer.infrastructure.cache;
 
 import java.time.Duration;
-import java.util.List;
 import java.util.Objects;
 
+import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
-import com.github.benmanes.caffeine.cache.LoadingCache;
 import edu.boardgames.collections.explorer.domain.BoardGameCollection;
-import edu.boardgames.collections.explorer.domain.DetailedBoardGameCollection;
 import edu.boardgames.collections.explorer.domain.GeekBuddy;
+import edu.boardgames.collections.explorer.domain.GeekBuddyCollectionFilter;
 import edu.boardgames.collections.explorer.domain.GeekBuddyCollections;
 
 public class GeekBuddyCollectionsCache implements GeekBuddyCollections {
-	private final LoadingCache<GeekBuddy, BoardGameCollection> ownedCollections;
-	private final LoadingCache<GeekBuddy, BoardGameCollection> wantToPlayCollections;
-	private final GeekBuddyCollections delegate;
+    private final Cache<String, BoardGameCollection> collections;
+    private final GeekBuddyCollections delegate;
 
 	public GeekBuddyCollectionsCache(GeekBuddyCollections delegate) {
-		this.delegate = Objects.requireNonNull(delegate);
-		this.ownedCollections = Caffeine.newBuilder()
-				.refreshAfterWrite(Duration.ofMinutes(5))
-				.build(delegate::owned);
-		this.wantToPlayCollections = Caffeine.newBuilder()
-				.refreshAfterWrite(Duration.ofMinutes(5))
-				.build(delegate::wantToPlay);
-	}
+        this.delegate = Objects.requireNonNull(delegate);
+        this.collections = Caffeine.newBuilder()
+                .expireAfterWrite(Duration.ofMinutes(5))
+                .build();
+    }
 
     @Override
     public BoardGameCollection complete(GeekBuddy geekBuddy) {
-        return delegate.complete(geekBuddy);
+        return collections.get(GeekBuddyCollectionFilter.NONE.id(geekBuddy), id -> delegate.complete(geekBuddy));
     }
 
     @Override
 	public BoardGameCollection owned(GeekBuddy geekBuddy) {
-		return Objects.requireNonNullElseGet(ownedCollections.get(geekBuddy), () -> emptyCollection(geekBuddy));
+        return collections.get(GeekBuddyCollectionFilter.OWNED.id(geekBuddy), id -> delegate.owned(geekBuddy));
 	}
 
     @Override
     public BoardGameCollection preordered(GeekBuddy geekBuddy) {
-        return delegate.preordered(geekBuddy);
+        return collections.get(GeekBuddyCollectionFilter.PREORDERED.id(geekBuddy), id -> delegate.preordered(geekBuddy));
     }
 
     @Override
     public BoardGameCollection rated(GeekBuddy geekBuddy) {
-        return delegate.rated(geekBuddy);
+        return collections.get(GeekBuddyCollectionFilter.RATED.id(geekBuddy), id -> delegate.rated(geekBuddy));
     }
 
     @Override
     public BoardGameCollection played(GeekBuddy geekBuddy) {
-        return delegate.played(geekBuddy);
+        return collections.get(GeekBuddyCollectionFilter.PLAYED.id(geekBuddy), id -> delegate.played(geekBuddy));
     }
 
     @Override
 	public BoardGameCollection wantToPlay(GeekBuddy geekBuddy) {
-		return Objects.requireNonNullElseGet(wantToPlayCollections.get(geekBuddy), () -> emptyCollection(geekBuddy));
+        return collections.get(GeekBuddyCollectionFilter.WANT_TO_PLAY.id(geekBuddy), id -> delegate.wantToPlay(geekBuddy));
 	}
 
 	@Override
 	public BoardGameCollection minimallyRated(GeekBuddy geekBuddy, int minrating) {
 		return delegate.minimallyRated(geekBuddy, minrating);
-	}
-
-	private BoardGameCollection emptyCollection(GeekBuddy geekBuddy) {
-		return new DetailedBoardGameCollection(geekBuddy.username(), geekBuddy.name(), List.of());
 	}
 }
