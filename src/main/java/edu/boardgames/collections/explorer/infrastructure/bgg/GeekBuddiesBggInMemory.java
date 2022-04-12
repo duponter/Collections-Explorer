db47221e4a0785811421c35311a0dcf2b04fcf1b
Erwin Dupont <erwin.dupont@aca-it.be>
2022-04-13 00:37:04 +0200

    GeekBuddies.one now returns the predefined GeekBuddy when it exists

--- src/main/java/edu/boardgames/collections/explorer/infrastructure/bgg/GeekBuddiesBggInMemory.java
+++ src/main/java/edu/boardgames/collections/explorer/infrastructure/bgg/GeekBuddiesBggInMemory.java
@@ -1,9 +1,12 @@
 package edu.boardgames.collections.explorer.infrastructure.bgg;
 
 import java.util.List;
+import java.util.Map;
 
 import edu.boardgames.collections.explorer.domain.GeekBuddies;
 import edu.boardgames.collections.explorer.domain.GeekBuddy;
+
+import static java.util.Map.entry;
 
 /*
 GeekBuddy groups:
@@ -15,29 +18,32 @@ sirplayalot
 mine: duponter + borrowed
  */
 public class GeekBuddiesBggInMemory implements GeekBuddies {
+    private static final Map<String, GeekBuddy> BUDDIES = Map.ofEntries(
+            entry("duponter", new GeekBuddyBgg("duponter", "Erwin Dupont")),
+            entry("jarrebesetoert", new GeekBuddyBgg("jarrebesetoert", "Koen Lostrie")),
+            entry("WouterAerts", new GeekBuddyBgg("WouterAerts", "Wouter Aerts")),
+            entry("bartie", new GeekBuddyBgg("bartie", "Bart De Vré")),
+            entry("de rode baron", new GeekBuddyBgg("de rode baron", "Steffen Wendelen")),
+            entry("Edou", new GeekBuddyBgg("Edou", "Edouard Van Belle")),
+            entry("evildee", new GeekBuddyBgg("evildee", "Didier De Breuck")),
+            entry("ForumMortsel", new GeekBuddyBgg("ForumMortsel", "FORUM Mortsel")),
+            entry("FFED", new GeekBuddyBgg("FFED", "FORUM Federatie")),
+            entry("Svennos", new GeekBuddyBgg("Svennos", "Sven Talboom")),
+            entry("TurtleR6", new GeekBuddyBgg("TurtleR6", "Dirk Frederickx")),
+            entry("engelwi", new GeekBuddyBgg("engelwi", "Wim Engels")),
+            entry("wallofshame", new GeekBuddyBgg("wallofshame", "Johan Drubbel")),
+            entry("leys", new GeekBuddyBgg("leys", "Pieter Leys")),
+            entry("dierenh", new GeekBuddyBgg("dierenh", "Hilde Vandierendonck"))
+
+    );
+
 	@Override
 	public List<GeekBuddy> all() {
-		return List.of(
-				new GeekBuddyBgg("duponter", "Erwin Dupont"),
-				new GeekBuddyBgg("jarrebesetoert", "Koen Lostrie"),
-				new GeekBuddyBgg("WouterAerts", "Wouter Aerts"),
-				new GeekBuddyBgg("bartie", "Bart De Vré"),
-				new GeekBuddyBgg("de rode baron", "Steffen Wendelen"),
-				new GeekBuddyBgg("Edou", "Edouard Van Belle"),
-				new GeekBuddyBgg("evildee", "Didier De Breuck"),
-				new GeekBuddyBgg("ForumMortsel", "FORUM Mortsel"),
-//				new GeekBuddyBgg("FFED", "FORUM Federatie"),
-				new GeekBuddyBgg("Svennos", "Sven Talboom"),
-				new GeekBuddyBgg("TurtleR6", "Dirk Frederickx"),
-				new GeekBuddyBgg("engelwi", "Wim Engels"),
-				new GeekBuddyBgg("wallofshame", "Johan Drubbel"),
-				new GeekBuddyBgg("leys", "Pieter Leys")
-//				new GeekBuddyBgg("dierenh", "Hilde Vandierendonck")
-		);
+        return List.copyOf(BUDDIES.values());
 	}
 
 	@Override
 	public GeekBuddy one(String username) {
-		return new GeekBuddyBgg(username, username);
+        return BUDDIES.getOrDefault(username, new GeekBuddyBgg(username, username));
 	}
 }
